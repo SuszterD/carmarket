9825387a9e87769bf539f702624dd58da02f278e
Daniel <d.suszter@gmail.com>
2026-03-04 21:12:29 +0100

    feat(frontend): add ListingService for API communication

---
+++ frontend/carmarket/src/app/services/listing.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { CarListing } from '../models/car-listing.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ListingService {
+  private apiUrl = 'http://localhost:8000/listings';
+
+  constructor(private http: HttpClient) {}
+
+  getListings(): Observable<CarListing[]> {
+    return this.http.get<CarListing[]>(this.apiUrl);
+  }
+}
