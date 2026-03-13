4029d07d5af145516bdae4881849ae43fce76eff
Daniel <d.suszter@gmail.com>
2026-03-13 04:56:32 +0100

    feat(frontend): implement create listing form

--- frontend/carmarket/src/app/features/listings/pages/listing-create-page/listing-create-page.ts
+++ frontend/carmarket/src/app/features/listings/pages/listing-create-page/listing-create-page.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-listing-create-page',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './listing-create-page.html',
   styleUrl: './listing-create-page.css',
 })
-export class ListingCreatePage {}
+export class ListingCreatePage {
+  form!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      brand: [''],
+      model: [''],
+      year: [''],
+      price: [''],
+      mileage: [''],
+      fuel_type: [''],
+      description: [''],
+    });
+  }
+}
