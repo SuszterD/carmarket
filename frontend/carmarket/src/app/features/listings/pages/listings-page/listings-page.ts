2e1b949b11dbe1fcf4223538b56f1b9cd7743ea6
Daniel <d.suszter@gmail.com>
2026-03-13 03:20:11 +0100

    feat(frontend): generate listings-page component

---
+++ frontend/carmarket/src/app/features/listings/pages/listings-page/listings-page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listings-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './listings-page.html',
+  styleUrl: './listings-page.css',
+})
+export class ListingsPage {}
