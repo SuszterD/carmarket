9b809337fbe08574ec06f8d8e13cc21e068747c3
Daniel <d.suszter@gmail.com>
2026-03-13 07:30:33 +0100

    feat(frontend): implement listing edit functionality

--- frontend/carmarket/src/app/features/listings/components/listing-card/listing-card.ts
+++ frontend/carmarket/src/app/features/listings/components/listing-card/listing-card.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarListing } from '../../models/car-listing.model';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-listing-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './listing-card.html',
   styleUrl: './listing-card.css',
 })
--- frontend/carmarket/src/app/features/listings/pages/listing-edit-page/listing-edit-page.ts
+++ frontend/carmarket/src/app/features/listings/pages/listing-edit-page/listing-edit-page.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { ListingService } from '../../services/listings.service';
 
 @Component({
   selector: 'app-listing-edit-page',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './listing-edit-page.html',
   styleUrl: './listing-edit-page.css',
 })
-export class ListingEditPage {}
+export class ListingEditPage {
+  form!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private listingService: ListingService,
+    private route: ActivatedRoute,
+    private router: Router,
+  ) {
+    this.form = this.fb.group({
+      brand: [''],
+      model: [''],
+      year: [''],
+      price: [''],
+      mileage: [''],
+      fuel_type: [''],
+      description: [''],
+    });
+  }
+
+  ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+
+    if (!id) return;
+
+    this.listingService.getListing(id).subscribe((listing) => {
+      this.form.patchValue(listing);
+    });
+  }
+
+  submit() {
+    const id = this.route.snapshot.paramMap.get('id');
+
+    if (!id) return;
+
+    this.listingService.updateListing(id, this.form.value).subscribe(() => {
+      this.router.navigate(['/listings']);
+    });
+  }
+}
